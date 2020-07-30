52c7e642a6049775365d1366411496ca16a3cc0b
ahkar1 <ahkar5@gmail.com>
2020-07-29 19:30:05 -0500

    Init Code Commit

---
+++ src/test/java/com/matthias/paintball/PaintballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthias.paintball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
